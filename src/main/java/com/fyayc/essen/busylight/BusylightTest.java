f422456cd3991ba3123c22d2de7b5745de128e0f
Chandra Tungathurthi <ctu@foryouandyourcustomers.com>
2019-08-07 12:55:37 +0200

    Adding documentation for the protocol

--- src/main/java/com/fyayc/essen/busylight/BusylightTest.java
+++ src/main/java/com/fyayc/essen/busylight/BusylightTest.java
@@ -6,7 +6,7 @@ import com.fyayc.essen.busylight.core.protocol.ProtocolConstants.Specs;
 public class BusylightTest {
   public static void main(String[] args) throws InterruptedException {
     try (Driver driver = Driver.tryAndAcquire()) {
-      driver.send(Specs.DO_NOT_DISTURB.protocol);
+      driver.send(Specs.FREE.protocol);
       // send keep alive signal
       while (true) {
         Thread.sleep(10_000);
