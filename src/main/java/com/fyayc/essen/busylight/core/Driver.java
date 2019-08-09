12e0928227d10658a9b94b4d7a0e53f15e75db43
Chandra Tungathurthi <ctu@foryouandyourcustomers.com>
2019-08-09 14:49:57 +0200

    pusing the annoying sending log message to debug level

--- src/main/java/com/fyayc/essen/busylight/core/Driver.java
+++ src/main/java/com/fyayc/essen/busylight/core/Driver.java
@@ -127,7 +127,7 @@ public class Driver implements Closeable {
    * @param buffer the buffer
    */
   public void send(StandardSpecs buffer) {
-    logger.info("Sending {}", buffer);
+    logger.debug("Sending {}", buffer);
     send(buffer.protocol);
   }
 
